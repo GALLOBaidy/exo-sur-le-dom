f55cdd03d5aed4c63cab6f711b34e2b20ad67336
Baïdy GALLO <gallo.baidy@gmail.com>
2024-11-15 16:32:31 +0100

    fin de l'exo

--- script/dom.js
+++ script/dom.js
@@ -3,13 +3,16 @@
 let lireDOM = document.getElementById("dom");
 let modifDOM = document.getElementById("dom_modif");
 let stl = document.getElementById("style");
+//On crée nos variables pour pouvoir appeler nos balises grâce à leurs Ids
+let titre1 = document.getElementById("titre1");
+let titre2 = document.getElementById("titre2");
+let titres = document.getElementsByClassName("titre");
+let para = document.getElementById("paragraphe1");
+let ul = document.getElementById("liste");
+let li = document.getElementsByTagName("li");
 
 //On crée notre première événement sur un événements click
 lireDOM.addEventListener("click", function () {
-  //On crée nos variables pour pouvoir appeler nos balises grâce à leurs Ids
-  let titre2 = document.getElementById("titre2");
-  let titres = document.getElementsByClassName("titre");
-  let li = document.getElementsByTagName("li");
   //Le String()constructeur crée des objets String. Lorsqu'il est appelé en tant que fonction, il renvoie des valeurs primitives de type String.
   let titleTextR = String();
   let listli = String();
@@ -43,3 +46,28 @@ lireDOM.addEventListener("click", function () {
       "\n"
   );
 });
+
+//On crée un événement dès que le bouton a été cliqué
+modifDOM.addEventListener("click", function () {
+  //les nouveaux éléments à afficher avec la propriété innertext
+  titre1.innerText = "Bienvenue ";
+  titre2.innerText = "Exo JS";
+  para.innerText = "Voici quelques fonctions exécutées en javascript.";
+  //On crée une boucle pour que la modification de la liste puisse tjrs s'adapter si on l'a rallonge ou on l'a raccourci
+  for (let i = 0; i < li.length; i++) {
+    li[i].innerHTML = "Liste " + (i + 1); //(i+1) pour que le comptage commence par 1
+  }
+});
+
+// on crée notre événement pour que le changement puisse se faire dès le click
+stl.addEventListener("click", function () {
+  //les changement que l'on souhaite faire
+  titre1.style.textAlign = "center";
+  //On crée une boucle pour que ça prenne tous les éléments ayant la même class
+  for(a=0;a< titres.length;a++){
+    titres[a].style.color = "red";
+  };
+  para.style.border = "solid 1px black";
+  para.style.padding = "10px";
+  ul.remove();
+})
